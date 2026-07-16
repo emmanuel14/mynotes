d4f2718db2223a4caf0ac51df192f54a285bb177
Emmanuel <akuwudikeemmanuel03@gmail.com>
2026-07-16 22:34:26 +0100

    Updates

--- lib/main.dart
+++ lib/main.dart
@@ -51,32 +51,3 @@ class HomePage extends StatelessWidget {
   }
 }
 
-class VerifyEmailView extends StatefulWidget {
-  const VerifyEmailView({super.key});
-
-  @override
-  State<VerifyEmailView> createState() => _VerifyEmailViewState();
-}
-
-class _VerifyEmailViewState extends State<VerifyEmailView> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Verify Email'),
-      ),
-      body: Column(
-        children: [
-      const Text('Please verify your email address'),
-      TextButton(
-        onPressed: () async {
-          final user = FirebaseAuth.instance.currentUser;
-          await user?.sendEmailVerification();
-        },
-        child: const Text('Send Email Verification'),
-      ),
-        ],
-      ),
-    );  
-  }
-}
