4519b64a1115a140f79694d1a137e48f20883eb1
Emmanuel <akuwudikeemmanuel03@gmail.com>
2026-07-20 08:56:04 +0100

    Updates

--- lib/views/register_view.dart
+++ lib/views/register_view.dart
@@ -1,6 +1,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'dart:developer' as devtools show log;
 
 
 class RegisterView extends StatefulWidget {
