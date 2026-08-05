feb07fe9c4dc0edea2ce36cd0d91fcdba6d60d57
Emmanuel <akuwudikeemmanuel03@gmail.com>
2026-08-05 16:13:48 +0100

    Updates

--- lib/views/notes/notes_view.dart
+++ lib/views/notes/notes_view.dart
@@ -23,11 +23,6 @@ class _NotesViewState extends State<NotesView> {
     super.initState();
   }
 
-  @override
-  void dispose() {
-    _notesService.close();
-    super.dispose();
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -78,7 +73,26 @@ future: _notesService.getOrCreateUser(email: userEmail),
             switch (snapshot.connectionState){
               case ConnectionState.waiting:
               case ConnectionState.active:
-               return const Text('Waiting for all notes...');
+              if (snapshot.hasData){
+                final allNotes = snapshot.data as List<DatabaseNote>;
+                return ListView.builder(
+                  itemCount: allNotes.length,
+                  itemBuilder: (context, index){
+                    final note = allNotes[index];
+                    return ListTile(
+                      title: Text(
+                        note.text,
+                        maxLines: 1,
+                        softWrap: true,
+                        overflow: TextOverflow.ellipsis,
+                        ),
+                    );
+                  },
+                ); 
+              } else {
+                return const CircularProgressIndicator();
+              }
+          
               default:
                 return const CircularProgressIndicator();
             }
