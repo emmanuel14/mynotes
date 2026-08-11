cbaa0194b9eec78a793a554530fdad50a094caf6
Emmanuel <akuwudikeemmanuel03@gmail.com>
2026-08-11 12:51:03 +0100

    Updates

---
+++ lib/services/cloud/cloud_note.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/foundation.dart';
+
+@immutable
+class CloudNote {
+  final String documentId;
+  final String ownerUserId;
+  final String text;
+  const CloudNote({
+    required this.documentId,
+    required this.ownerUserId,
+    required this.text,
+  });
+
+}
