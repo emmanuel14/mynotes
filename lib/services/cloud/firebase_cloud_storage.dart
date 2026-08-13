40b543dd891efe1913697823543bb103d41b0591
Emmanuel <akuwudikeemmanuel03@gmail.com>
2026-08-13 12:17:46 +0100

    Updates

--- lib/services/cloud/firebase_cloud_storage.dart
+++ lib/services/cloud/firebase_cloud_storage.dart
@@ -4,26 +4,48 @@ import 'package:mynotes/services/cloud/cloud_storage_constants.dart';
 import 'package:mynotes/services/cloud/cloud_storage_exceptions.dart';
 
 class FirebaseCloudStorage {
-
   final notes = FirebaseFirestore.instance.collection('notes');
 
-  Future<CloudNote> getNote({required String documentId}) async {
-    try {
-      final snapshot = await notes.doc(documentId).get();
-      if (snapshot.exists){
-        return CloudNote.fromSnapshot(snapshot as QueryDocumentSnapshot<Map<String, dynamic>>);
-      } else {
-        throw CouldNotFindNoteException();
-      }
-    } catch (e) {
-      throw CouldNotGetNoteException();
+  Future<void> updateNote({
+    required String documentId,
+    required String text
+  }) async {
+    try{
+      notes.doc(documentId).update({textFieldName: text});
+    } catch (e){
+      throw CouldNotUpdateNoteException();
     }
   }
 
-  void createNewNote({required String ownerUserId, required String text}) async {
-    final document = await notes.add({
+  Stream<Iterable<CloudNote>> allNotes({required String ownerUserId}) =>
+    notes.snapshots().map((event) => event.docs.map((doc) => CloudNote.fromSnapshot(doc))
+    .where((note) => note.ownerUserId == ownerUserId));
+
+  Future<Iterable<CloudNote>> getNotes({required String ownerUserId}) async {
+    try {
+      return await notes.where(
+        ownerUserIdFieldName,
+        isEqualTo: ownerUserId
+      )
+      .get()
+      .then(
+        (value) => value.docs.map(
+          (doc){
+            return CloudNote(
+              documentId: doc.id, 
+              ownerUserId: doc.data() [ownerUserIdFieldName] as String, 
+              text: doc.data()[textFieldName] as String,
+          );
+      }));
+    } catch (e) {
+      throw CouldNotGetAllNotesException();
+    }
+  }
+
+  void createNewNote({required String ownerUserId}) async {
+      await notes.add({
       ownerUserIdFieldName: ownerUserId,
-      textFieldName: text,
+      textFieldName: '',
     });
   }
 
