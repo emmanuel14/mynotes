4291ad946a7efe6fa9e729b0fd4a4300e852953a
Emmanuel <akuwudikeemmanuel03@gmail.com>
2026-08-26 14:46:43 +0100

    Updates

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mynotes/services/auth/auth_service.dart';
 import 'package:mynotes/views/login_view.dart';
 import 'package:mynotes/views/notes/create_update_note_view.dart';
--- lib/views/notes/create_update_note_view.dart
+++ lib/views/notes/create_update_note_view.dart
@@ -6,7 +6,7 @@ import 'package:mynotes/services/cloud/cloud_storage_exceptions.dart';
 import 'package:mynotes/services/cloud/firebase_cloud_storage.dart';
 import 'package:mynotes/utilities/dialogs/cannot_share_empty_note_dialog.dart';
 import 'package:mynotes/utilities/generics/get_arguments.dart';
-import 'package:share/share.dart';
+import 'package:share_plus/share_plus.dart';
 
 class CreateUpdateNoteView extends StatefulWidget {
   const CreateUpdateNoteView({super.key});
@@ -103,7 +103,9 @@ class _CreateUpdateNoteViewState extends State<CreateUpdateNoteView> {
               if(_note == null || text.isEmpty){
                 await showCannotShareEmptyNoteDialog(context);
               } else{
-                Share.share(text);
+                await SharePlus.instance.share(
+                  ShareParams(text: text),
+                  );
               }
             }, 
             icon: const Icon(Icons.share),
