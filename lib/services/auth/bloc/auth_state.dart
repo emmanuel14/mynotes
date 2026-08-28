22d7792f969b9609c3a523ef2f93add526064849
Emmanuel <akuwudikeemmanuel03@gmail.com>
2026-08-28 12:07:29 +0100

    Updates

--- lib/services/auth/bloc/auth_state.dart
+++ lib/services/auth/bloc/auth_state.dart
@@ -26,12 +26,16 @@ class AuthStateNeedsVerification extends AuthState{
   const AuthStateNeedsVerification();
 }
 
-class AuthStateLoggedOut extends AuthState{
+class AuthStateLoggedOut extends AuthState with EquatableMixin{
   final Exception? exception;
   final bool isLoading;
   const AuthStateLoggedOut({
     required this.exception, 
     required this.isLoading,
 });
+
+  @override
+  List<Object?> get props => [exception, isLoading];
 }
+
 
