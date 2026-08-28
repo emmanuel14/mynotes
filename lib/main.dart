8aaceda757964a8fee9339d0aecced04153d9e68
Emmanuel <akuwudikeemmanuel03@gmail.com>
2026-08-28 12:18:06 +0100

    Updates

--- lib/main.dart
+++ lib/main.dart
@@ -24,10 +24,7 @@ void main() {
         child: HomePage(),
       ),
       routes: {
-        loginRoute: (context) => const LoginView(),
-        registerRoute: (context) => const RegisterView(),
-        notesRoute: (context) => const NotesView(),
-        verifyEmailRoute:(context) => VerifyEmailView(),
+
         createOrUpdateNoteRoute:(context) => const CreateUpdateNoteView(),
       },
     )
@@ -48,6 +45,8 @@ class HomePage extends StatelessWidget {
         return const VerifyEmailView();
       } else if (state is AuthStateLoggedOut){
         return const LoginView();
+      } else if (state is AuthStateRegistering){
+        return const RegisterView(); 
       } else {
         return const Scaffold(
           body: CircularProgressIndicator(),
